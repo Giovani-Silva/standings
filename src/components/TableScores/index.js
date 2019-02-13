b27f1de7e690fe9bdbca4b1a1cf53d10bfe071bd
Giovani Silva <giovani.silva@live.com>
2019-02-13 15:58:57 -0200

    Spring Animation

--- src/components/TableScores/index.js
+++ src/components/TableScores/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
+import { Spring } from 'react-spring/renderprops';
 
 import { Creators as StandingsActions } from '../../store/ducks/standings';
 
@@ -40,43 +41,50 @@ class TableScores extends Component {
     const items = standings.filter(total => total.type === 'TOTAL');
 
     return items.map(item => (
-      <Scores key={new Date().getTime()}>
-        <thead>
-          {!!item.group && (
-            <tr>
-              <th colSpan="10">{item.group.split('_').join(' ')}</th>
-            </tr>
-          )}
-          <tr>
-            <th scope="col">*</th>
-            <th scope="col">TEAM</th>
-            <th scope="col">PTS</th>
-            <th scope="col">PG</th>
-            <th scope="col">W</th>
-            <th scope="col">D</th>
-            <th scope="col">L</th>
-            <th scope="col">GF</th>
-            <th scope="col">GA</th>
-            <th scope="col">GD</th>
-          </tr>
-        </thead>
-        <tbody>
-          {item.table.map(time => (
-            <tr key={time.team.id}>
-              <td>{time.position}</td>
-              <td>{time.team.name}</td>
-              <td>{time.points}</td>
-              <td>{time.playedGames}</td>
-              <td>{time.won}</td>
-              <td>{time.draw}</td>
-              <td>{time.lost}</td>
-              <td>{time.goalsFor}</td>
-              <td>{time.goalsAgainst}</td>
-              <td>{time.goalDifference}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Scores>
+      <Spring
+        from={{ opacity: 0, transform: 'translateY(10%)' }}
+        to={{ opacity: 1, transform: 'translateY(0)' }}
+      >
+        {props => (
+          <Scores style={props} key={new Date().getTime()}>
+            <thead>
+              {!!item.group && (
+                <tr>
+                  <th colSpan="10">{item.group.split('_').join(' ')}</th>
+                </tr>
+              )}
+              <tr>
+                <th scope="col">*</th>
+                <th scope="col">TEAM</th>
+                <th scope="col">PTS</th>
+                <th scope="col">PG</th>
+                <th scope="col">W</th>
+                <th scope="col">D</th>
+                <th scope="col">L</th>
+                <th scope="col">GF</th>
+                <th scope="col">GA</th>
+                <th scope="col">GD</th>
+              </tr>
+            </thead>
+            <tbody>
+              {item.table.map(time => (
+                <tr key={time.team.id}>
+                  <td>{time.position}</td>
+                  <td>{time.team.name}</td>
+                  <td>{time.points}</td>
+                  <td>{time.playedGames}</td>
+                  <td>{time.won}</td>
+                  <td>{time.draw}</td>
+                  <td>{time.lost}</td>
+                  <td>{time.goalsFor}</td>
+                  <td>{time.goalsAgainst}</td>
+                  <td>{time.goalDifference}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Scores>
+        )}
+      </Spring>
     ));
   };
 
@@ -90,8 +98,12 @@ class TableScores extends Component {
 
     return (
       <Container>
-        <h3>{`${competition.name} - ${competition.area.name}`}</h3>
-        <h4>{seasonNow}</h4>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {props => <h3 style={props}>{`${competition.name} - ${competition.area.name}`}</h3>}
+        </Spring>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {props => <h4 style={props}>{seasonNow}</h4>}
+        </Spring>
         {this.renderTable()}
       </Container>
     );
